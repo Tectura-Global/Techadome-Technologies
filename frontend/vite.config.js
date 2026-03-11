148f7a82bc1fca913e8775fc69d7ebf63d74725c
Rhulani <rhulanimogotsi@gmail.com>
2026-03-11 15:37:26 +0200

    MORE vercel fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
-    allowedHosts:
-      [
-        'highly-increasing-ashton.ngrok-free.dev'
-      ]
+    allowedHosts: ['highly-increasing-ashton.ngrok-free.dev']
   }
 })
