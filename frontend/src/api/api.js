f6a53361850e5df6143d6e5de075bfbb170df129
Kgosi <rhulani@tecturaglobal.com>
2026-06-10 12:07:47 +0200

    CORS updates

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import Cookies from 'js-cookie'
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || '/',
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 });
 
